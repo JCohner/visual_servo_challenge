3d8fb39bf10026bb7990d9330472cade3cf762ea
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-16 17:27:43 -0500

    working number based command of position

--- visual_servo.py
+++ visual_servo.py
@@ -6,36 +6,23 @@ class visual_servo():
 		print("initializing serial port")
 		self.ser = serial.Serial(port = '/dev/ttyACM0', baudrate = 9600, timeout = 5)
 		print("serial port open!")
-		self.set_position(int(sys.argv[1]))
+		self.set_position(float(sys.argv[1]))
 		self.close()
 
 	def set_position(self, pos):
 		#turn pos from 0 - 100 to 1000-2000
-		ms = int(1000 + (pos/100.0 * 1000))
-		print(ms)
+		ms = int(1000 + (pos/100.0 * 1000)) * 4
 		bin_ms = bin(ms)
-		print(bin_ms)
-		low_bin = bin_ms[-8:-1]
-		high_bin = bin_ms[2:-8]
-		pad = ''.zfill(7 - len(high_bin))
-		high_bin = pad + high_bin
 
+		low_bin = ms & 0b1111111
+		high_bin = (ms & 0b11111110000000) >> 7
 
-		print(hex(int(low_bin, 2)))
-		print(hex(int(high_bin, 2)))
-
-		low_bin = chr(int(low_bin, 2))
-		print(hex(ord(low_bin)))
-
-		high_bin = chr(int(high_bin, 2))
-		print(hex(ord(high_bin)))
+		low_bits = chr(low_bin)
+		high_bits = chr(high_bin)
 
 		set_pos_command = chr(0x84)
 		pick_motor_command = chr(0x0)
-		print(set_pos_command+pick_motor_command)
-		print(hex(ord(set_pos_command)) + hex(ord(pick_motor_command)) + hex(ord(low_bin)) + hex(ord(high_bin)))
-		#self.ser.write(set_pos_command + pick_motor_command + chr(0x70) + chr(0x2E))
-		self.ser.write("\x84\x00\x70\x2E")
+		self.ser.write(set_pos_command + pick_motor_command + low_bits + high_bits)
 
 
 
