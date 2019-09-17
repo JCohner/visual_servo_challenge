74bdfa0cc9245047e5c2e00b84cdfa5d66ff6add
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-17 13:26:34 -0500

    made classfull and added command line arguments for saving and replaying videos

--- video_capture.py
+++ video_capture.py
@@ -1,37 +1,59 @@
 import cv2
 import numpy as np
 import time
+import argparse
 
-cap = cv2.VideoCapture('/dev/video2')
-camera_freq = cap.get(cv2.CAP_PROP_FPS)
-print(camera_freq)
+#argument parsing
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-bottom_left_corner = (10, 440)
-font_color = (255,255,255)
-line_type = 2
+class Camera():
+	def __init__(self):
+		self.parse_args()
 
-while(True):
-	loop_time = time.time()
-	
-	ret, frame = cap.read()
+		cap = cv2.VideoCapture('/dev/video2')
+		camera_freq = cap.get(cv2.CAP_PROP_FPS)
+		# self.img_width = cap.get(cv2.CAP_PROP_WIDTH)
+		# self.img_height = cap.get(cv2.CAP_PROP_HEIGHT)		
+		self.cap = cap
 
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	lower_red = np.array([0, 127, 127])
-	upper_red = np.array([10, 255, 255])
+		self.font = cv2.FONT_HERSHEY_SIMPLEX
+		self.bottom_left_corner = (10, 440)
+		self.font_color = (255,255,255)
+		self.line_type = 2
 
-	mask = cv2.inRange(hsv, lower_red, upper_red)
+		self.camera_loop()
 
-	res = cv2.bitwise_and(frame, frame, mask = mask)
+	def parse_args(self):
+		parser = argparse.ArgumentParser()
+		group = parser.add_mutually_exclusive_group()
+		group.add_argument("-s","--save_recording", help="record capture", action = "store_true")
+		group.add_argument("-p", "--play_recorded", help="use recroded capture", action= "store_true")
 
-	loop_time = time.time() - loop_time
-	cv2.putText(hsv, str(1/loop_time), bottom_left_corner,font, 1, font_color, line_type)
+
+	def camera_loop(self):
+		while(True):
+			loop_time = time.time()
+			
+			ret, frame = self.cap.read()
+
+			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+			lower_red = np.array([0, 127, 127])
+			upper_red = np.array([10, 255, 255])
+
+			mask = cv2.inRange(hsv, lower_red, upper_red)
+
+			res = cv2.bitwise_and(frame, frame, mask = mask)
+
+			loop_time = time.time() - loop_time
+			cv2.putText(hsv, str(1/loop_time), self.bottom_left_corner, self.font, 1, self.font_color, self.line_type)
 
 
-	cv2.imshow('frame', res)
+			cv2.imshow('frame', res)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
 
-cap.release()
-cv2.destroyAllWindows()
+		cap.release()
+		cv2.destroyAllWindows()		
+
+#calling it as its own main for now, will be invoked by servo or some other parent later
+cam = Camera()
