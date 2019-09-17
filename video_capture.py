88f48b0ff35be3e1e5e79421365d759a0ffe0331
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-17 11:40:13 -0500

    suitable red masking video capture, will work on implementing frame grabbing and plotting center of ball

--- video_capture.py
+++ video_capture.py
@@ -16,15 +16,19 @@ while(True):
 	
 	ret, frame = cap.read()
 
-	
-	grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_red = np.array([0, 127, 127])
+	upper_red = np.array([10, 255, 255])
+
+	mask = cv2.inRange(hsv, lower_red, upper_red)
+
+	res = cv2.bitwise_and(frame, frame, mask = mask)
 
 	loop_time = time.time() - loop_time
+	cv2.putText(hsv, str(1/loop_time), bottom_left_corner,font, 1, font_color, line_type)
 
-	cv2.putText(grey, str(1/loop_time), bottom_left_corner,font, 1, font_color, line_type)
 
-
-	cv2.imshow('frame', grey)
+	cv2.imshow('frame', res)
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
