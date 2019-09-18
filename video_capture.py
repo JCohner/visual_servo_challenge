d513f070db7a9b3b87f30e4c469726928f525ded
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-18 11:06:23 -0500

    broke out frame reading and motion control loop into their seperate functions both called by the main loop. only position implemented in x direction, at fixed velocity. must extrapolate to y direction as well as implement proportional velocity. further optimizations on cv ball detection can also be made

--- video_capture.py
+++ video_capture.py
@@ -29,7 +29,7 @@ class Camera():
 		self.line_type = 2
 
 	def mask_init(self):
-		self.lower_red = np.array([30, 100, 127])
+		self.lower_red = np.array([20, 100, 100])
 		self.upper_red = np.array([40, 255, 255])
 
 	def parse_args(self):
@@ -45,74 +45,80 @@ class Camera():
 		elif (args.play_recorded):
 			return 'play'
 
-	def move_camera_center(self, x,y):
-		frame_x_err = self.img_center[0] - x
-		frame_y_err = self.img_center[1] - y
+	def move_camera_center(self, pos):
+		frame_x_err = -1 * (self.img_center[0] - pos[0])
+		frame_y_err = self.img_center[1] - pos[1]
 
 		servo1_pos = self.servo_controller.servo1.get_position()
 		servo2_pos = self.servo_controller.servo2.get_position()
 
+		self.servo_controller.servo1.set_speed(10)
+
+		if (frame_x_err > 5 or frame_x_err < -5):
+			mapped_error = frame_x_err/float(self.img_width) * 1000.0
+			print("current servo position: " + str(servo1_pos))
+			print("frame_x_err: " + str(frame_x_err))
+			print("mapped error: " + str(mapped_error))
+			self.servo_controller.servo1.go_to_position(servo1_pos + int(mapped_error))
+
+
+
+	def frame_read(self):
+		loop_time = time.time()
+		#pdb.set_trace()
+		ret, frame = self.cap.read()
+		#convert numpy matrix to represent HSV matrix
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		#make a mask that has low red and upper red hue bounds
+		mask = cv2.inRange(hsv, self.lower_red, self.upper_red)
+		#apply mask to our image
+		res = cv2.bitwise_and(hsv, hsv, mask = mask)
+		#split up resultant 3 channel image
+		[h , s, v] = cv2.split(res)
+		#combine to get single channel gray scale
+		thresh = cv2.bitwise_or(s, v)
+		#binary threshold the image
+		ret,thresh = cv2.threshold(thresh, 127, 255, 0)
+		#find the edges of the binary image
+		im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+
+		frame = cv2.GaussianBlur(frame, (5,5), 0)
+
+		erosion_size = 5
+		erosion_type = cv2.MORPH_ELLIPSE 
+		element = cv2.getStructuringElement(erosion_type, (2*erosion_size + 1, 2*erosion_size+1), (erosion_size, erosion_size))
+		frame = cv2.erode(frame, element)
+
+		frame = cv2.drawContours(frame, contours, -1, color = (255,0,0), thickness = 3)
+		
+		#get largest contour
+		mid_x = None
+		mid_y = None
+		if len(contours) != 0:
+			c = max(contours, key = cv2.contourArea)
+			#check if this is actually circularizing like we think it is
+			convex_hull = cv2.convexHull(c)
+			cv2.drawContours(frame, convex_hull, -1, color = (255,0,0), thickness = 3)
+			x,y,w,h = cv2.boundingRect(convex_hull)
+
+			if isinstance(x, int) and isinstance(y,int):
+				#print((x,y))
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+	    		mid_x = int(x + w/2)
+	    		mid_y = int(y + h/2)
+
+	    		cv2.line(frame, (mid_x,0), (mid_x, self.img_height), (0,255,0), 2)
+	    		cv2.line(frame, (0,mid_y), (self.img_width, mid_y), (0,255,0), 2)
+	    		cv2.putText(frame, str((mid_x,mid_y)), self.bottom_left_corner, self.font, 1, self.font_color, self.line_type)
+		cv2.imshow('frame', frame)
+		return (mid_x, mid_y)
 
 	def camera_loop(self):
 		while(True):
-			loop_time = time.time()
-			#pdb.set_trace()
-			ret, frame = self.cap.read()
-			#convert numpy matrix to represent HSV matrix
-			hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-			#make a mask that has low red and upper red hue bounds
-			mask = cv2.inRange(hsv, self.lower_red, self.upper_red)
-			#apply mask to our image
-			res = cv2.bitwise_and(hsv, hsv, mask = mask)
-			#split up resultant 3 channel image
-			[h , s, v] = cv2.split(res)
-			#combine to get single channel gray scale
-			thresh = cv2.bitwise_or(s, v)
-			#binary threshold the image
-			ret,thresh = cv2.threshold(thresh, 127, 255, 0)
-			#find the edges of the binary image
-			im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-
-			frame = cv2.GaussianBlur(frame, (5,5), 0)
-
-			erosion_size = 5
-			erosion_type = cv2.MORPH_ELLIPSE 
-			element = cv2.getStructuringElement(erosion_type, (2*erosion_size + 1, 2*erosion_size+1), (erosion_size, erosion_size))
-			frame = cv2.erode(frame, element)
-
-			frame = cv2.drawContours(frame, contours, -1, color = (255,0,0), thickness = 3)
-			
-			#get largest contour
-			if len(contours) != 0:
-				c = max(contours, key = cv2.contourArea)
-				#check if this is actually circularizing like we think it is
-				convex_hull = cv2.convexHull(c)
-				#cv2.drawContours(frame, convex_hull, -1, color = (255,0,0), thickness = 3)
-				x,y,w,h = cv2.boundingRect(convex_hull)
-
-				if isinstance(x, int) and isinstance(y,int):
-					#print((x,y))
-					cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-		    		mid_x = int(x + w/2)
-		    		mid_y = int(y + h/2)
-
-		    		cv2.line(frame, (mid_x,0), (mid_x, self.img_height), (0,255,0), 2)
-		    		cv2.line(frame, (0,mid_y), (self.img_width, mid_y), (0,255,0), 2)
-		    		cv2.putText(frame, str((mid_x,mid_y)), self.bottom_left_corner, self.font, 1, self.font_color, self.line_type)
-
-	    			self.move_camera_center(mid_x, mid_y)
-
-			# #erode to try further isolate noise
-			
-
-			# #blur image to low_pass filter
-			# blur = cv2.GaussianBlur(frame, (11,11), 0)
-
-			# #get convex 
-			# loop_time = time.time() - loop_time
-			# cv2.putText(blur, str(1/loop_time), self.bottom_left_corner, self.font, 1, self.font_color, self.line_type)
-
-			cv2.imshow('frame', frame)
+			ball_pos = self.frame_read()
+			print(ball_pos)
+			if ball_pos != (None, None):
+				self.move_camera_center(ball_pos)
 
 			if cv2.waitKey(1) & 0xFF == ord('q'):
 				break
