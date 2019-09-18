4be756aa6723b4c40808328af0fb7ed07a4111b7
JCohner <joshuacohen2019@u.northwestern.edu>
2019-09-18 16:57:05 -0500

    added Houghs Circle check, this can be further refined

--- take_screen_shot.py
+++ take_screen_shot.py
@@ -8,7 +8,7 @@ img_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 img_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 img_center = (img_width/2, img_height/2)		
 
-lower_red = np.array([15, 15, 200])
+lower_red = np.array([15, 50, 230])
 upper_red = np.array([40, 155, 255])
 
 #cv2.createTrackbar()
@@ -29,17 +29,31 @@ while (True):
 	thresh = cv2.bitwise_or(s, v)
 	#binary threshold the image
 	ret,thresh = cv2.threshold(thresh, 127, 255, 0)
+	# print(thresh.shape)
 	#find the edges of the binary image
 	im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
 
 	frame = cv2.GaussianBlur(frame, (5,5), 0)
-
+	# print(frame.shape)
 	erosion_size = 5
 	erosion_type = cv2.MORPH_ELLIPSE 
 	element = cv2.getStructuringElement(erosion_type, (2*erosion_size + 1, 2*erosion_size+1), (erosion_size, erosion_size))
 	frame = cv2.erode(frame, element)
-
+	# print(frame.shape)
 	frame = cv2.drawContours(frame, contours, -1, color = (255,0,0), thickness = 3)
+	# print(frame.shape)
+	rows = frame.shape[0]
+	circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, rows/8, param1 = 100, param2=30, minRadius = 1, maxRadius = 30)
+
+	if circles is not None:
+		circles = np.unint16(np.around(circles))
+		for i in circles[0,:]:
+			print("drawing circles")
+			center = (i[0], i[1])
+			cv2.circle(frame, center, 1, (0, 100, 100), 3)
+			radius = i[2]
+			cv2.circle(frame, center, radius, (255,0,255), 3)
+
 
 	cv2.imshow('frame', frame)
 
